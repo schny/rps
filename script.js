270bf759ed6a2fbd87c3347e04b2a8e06cb2b431
schny <mdschneider1@gmail.com>
2021-07-11 18:40:49 -0500

    added color to score-keeping

--- script.js
+++ script.js
@@ -17,6 +17,8 @@ function playGame (move) {
     userMove = move;
 
     let userValue;
+    let roundWinner;
+
     if (userMove =='rock') {
         userValue = 0;
     }
@@ -36,22 +38,26 @@ function playGame (move) {
         console.log("It's a tie!");
         callGame('user', 'computer', userMove, compMove);
         round = calcRound(round);
-        printRound(round, userMove, compMove);
+        roundWinner = 'tie';
+        printRound(round, userMove, compMove, roundWinner);
     }
     else if ( ((userValue > compValue) && (userValue - compValue != 2) || (userValue - compValue == -2) )) {
         callGame('user', 'computer', userMove, compMove);
         userScore = calcScore(userScore);
         printScore(userScore, computerScore);
         round = calcRound(round);
-        printRound(round, userMove, compMove);
-        console.log('poop');
+        roundWinner = 'user';
+        printRound(round, userMove, compMove, roundWinner);
+        
     }
     else {
         callGame('computer', 'user', compMove, userMove);
         computerScore = calcScore(computerScore);
+        roundWinner = 'computer';
         printScore(userScore, computerScore);
         round = calcRound(round);
-        printRound(round, userMove, compMove);
+        printRound(round, userMove, compMove, roundWinner);
+       
     }
 }
 
@@ -72,22 +78,31 @@ function calcRound(round) {
     return round;
 }
 
-function printRound(round, userMove, computerMove) {
+function printRound(round, userMove, computerMove, winner) {
     let roundTable = document.getElementById('rounds');
     let row = roundTable.insertRow(round - 1);
     let cell1 = row.insertCell(0);
     let cell2 = row.insertCell(1);
     let cell3 = row.insertCell(2);
+    // pick up here
     cell1.innerHTML = `${round}`;
+    if (winner == 'user') {
+        cell2.style.backgroundColor = 'rgb(144,238,144)';
+    }
+    else if (winner == 'computer') {
+        cell3.style.backgroundColor = 'rgb(144,238,144)';
+    }
+
     cell2.innerHTML = `<i class="far fa-hand-${userMove}"></i>`;
     cell3.innerHTML = `<i class="far fa-hand-${computerMove}"></i>`;
+
 }
 
 
 function callGame(winner, loser, winnerMove, loserMove) {
     let loserSentValue = getRandomInt(0,3);
     let winnerSentValue = getRandomInt(0,3);
-    let loserSentArray = [`The ${loser} tried desperately to fight with ${loserMove}, but it's futile.`,
+    let loserSentArray = [`The ${loser} tried desperately to fight with ${loserMove}, but it's no use.`,
                         `The ${loser} makes an attempted sneak attack with ${loserMove}, but the ${winner} sees it coming.`,
                     `After much thought, the ${loser} throws ${loserMove}. Little did he know what was waiting for him.`,
                     `The ${loser} throws up his hands and recklessly throws ${loserMove}.`]
